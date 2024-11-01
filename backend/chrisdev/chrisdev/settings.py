f34004aab1f6ff9bc1fbd0080c44891c378a790a
Samuel Osondu <samuelosondu.py@gmail.com>
2024-11-01 22:48:12 +0100

    added the email

--- backend/chrisdev/chrisdev/settings.py
+++ backend/chrisdev/chrisdev/settings.py
@@ -151,7 +151,16 @@ SIMPLE_JWT = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'samuelemailsend@gmail.com'
+EMAIL_HOST_PASSWORD = 'elwpxjsccelroeuw'
+DEFAULT_FROM_EMAIL = 'samuelemailsend@gmail.com'
 
 
 # CKEDITOR
